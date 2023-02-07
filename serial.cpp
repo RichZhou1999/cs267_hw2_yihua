07e51857405cbe3b26b926db6c5ca7b9ca45c2c8
RichZhou1999 <373072096@qq.com>
2023-02-07 11:16:38 -0800

    1

--- serial.cpp
+++ serial.cpp
@@ -101,7 +101,7 @@ void apply_force_bin(int row, int column, int p, particle_t* parts){
         return;
     }
     for (auto it = bins[column+row*lda].begin(); it != bins[column+row*lda].end(); ++it){
-        apply_force(parts[p], parts[it]);
+        apply_force(parts[p], parts[*it]);
     }
 
 }
@@ -120,17 +120,17 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     for(int i = 0; i < lda; ++i){
         for (int j = 0; j < lda; ++j){
             for (auto it = bins[i+j*lda].begin(); it != bins[i+j*lda].end(); ++it){
-                apply_force_bin(j-1,i-1, it, parts);
-                apply_force_bin(j-1,i, it, parts);
-                apply_force_bin(j-1,i+1, it, parts);
-                apply_force_bin(j,i-1, it, parts);
-                apply_force_bin(j,i+1, it, parts);
-                apply_force_bin(j+1,i-1, it, parts);
-                apply_force_bin(j+1,i, it, parts);
-                apply_force_bin(j+1,i+1, it, parts);
+                apply_force_bin(j-1,i-1, *it, parts);
+                apply_force_bin(j-1,i, *it, parts);
+                apply_force_bin(j-1,i+1, *it, parts);
+                apply_force_bin(j,i-1, *it, parts);
+                apply_force_bin(j,i+1, *it, parts);
+                apply_force_bin(j+1,i-1, *it, parts);
+                apply_force_bin(j+1,i, *it, parts);
+                apply_force_bin(j+1,i+1, *it, parts);
                 for (auto it2 = bins[i+j*lda].begin(); it2 != bins[i+j*lda].end(); ++it2){
                     if(it2 != it){
-                        apply_force(parts[it], parts[it2];
+                        apply_force(parts[*it], parts[*it2];
                     }
                 }
             }
